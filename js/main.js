33819bc348564f5f7d42cd0da774faedb6aa8ccc
Andrea Prina <and.prina@gmail.com>
2022-07-13 15:47:40 +0200

    feat: :sparkles: Create function to get n random email addresses from API

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 const app = new Vue(
     {
-        el : "#app"
+        el : "#app",
+
+        data : {
+
+            mailingList : [],
+        },
+
+        methods : {
+
+            createMailingList : function(itemNumber){
+                
+                for(let i = 0; i < itemNumber; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        const randomEmail = response.data.response;
+                        this.mailingList.push(randomEmail);
+                    })
+                }
+            }
+
+        },
+
+        created(){
+            this.createMailingList(5);
+            console.log(this.mailingList);
+        }
     }
 )
